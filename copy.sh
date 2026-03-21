f89b7be1f3e898467c49eb97145650f622c35a16
amitpadhan525 <amitpadhan525@gmail.com>
2026-03-21 22:39:21 +0530

    adding the command to copy the kitty's config

--- copy.sh
+++ copy.sh
@@ -3,3 +3,5 @@
 rsync -av --delete ~/.config/hypr/ ~/github/linux-dotfiles/hyprland/hypr
 rsync -av --delete ~/.config/waybar/ ~/github/linux-dotfiles/hyprland/waybar
 rsync -av --delete ~/.config/rofi/ ~/github/linux-dotfiles/hyprland/rofi
+rsync -av --delete ~/.config/kitty/ ~/github/linux-dotfiles/hyprland/kitty
+

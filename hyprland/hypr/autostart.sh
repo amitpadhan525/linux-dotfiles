c40891f4082686c528b66c64f107edae663df3ee
amitpadhan525 <amitpadhan525@gmail.com>
2026-03-27 19:19:30 +0530

    bug fixed - 2026-03-27 19:19:30

--- hyprland/hypr/autostart.sh
+++ hyprland/hypr/autostart.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-exit 0
-exec-once = /usr/bin/gnome-keyring-daemon --start --components=secrets
+/usr/bin/gnome-keyring-daemon --start --components=secrets &
 brave --enable-features=UseOzonePlatform --ozone-platform=wayland &

7712a54a6226400283d2e76e3df1a388c31796ff
amitpadhan525 <amitpadhan525@gmail.com>
2026-04-07 17:31:23 +0530

    some theme related changes not major changes only colurs - 2026-04-07 17:31:23

--- hyprland/hypr/scripts/wallpaper.sh
+++ hyprland/hypr/scripts/wallpaper.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+# ----------------------------------------------------- 
+# Wallpaper Script
+# ----------------------------------------------------- 
+
 pkill hyprpaper
 sleep 1
 hyprpaper &
 sleep 2
-hyprctl hyprpaper preload "/home/amit/.config/hypr/spiderman.jpeg"
-hyprctl hyprpaper wallpaper "eDP-1,/home/amit/.config/hypr/spiderman.jpeg"
+
+# Apply wallpaper using IPC
+hyprctl hyprpaper wallpaper "eDP-1,/home/amit/.config/hypr/bg.jpg" || true
+hyprctl hyprpaper wallpaper "HDMI-A-1,/home/amit/.config/hypr/bg.jpg" || true

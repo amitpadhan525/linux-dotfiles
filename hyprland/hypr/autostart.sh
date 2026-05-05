1ff877a08765a7df96c2cb64feb66f2aca5aeab7
amitpadhan525 <amitpadhan525@gmail.com>
2026-05-05 20:58:08 +0530

    some changes related on dark theme and trun on the hyprlock for lockscreen - 2026-05-05 20:58:08

--- hyprland/hypr/autostart.sh
+++ hyprland/hypr/autostart.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Autostart scripts for Hyprland
 # (Add any other background services here)
+
+# Enforce system-wide dark mode properly
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'

8374c36429f0ea04c8bc4d2cabd1b3471fc27693
amitpadhan525 <amitpadhan525@gmail.com>
2026-05-19 14:13:58 +0530

    CREATING moden look lockscreen - 2026-05-19 14:13:58

---
+++ hyprland/hypr/colors.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Dynamic colors generated from active wallpaper
+
+accent='#edf8f9'
+accent_two='#c2c6cc'
+bg_color='#0f0f0f'
+text_color='#ffffff'
+secondary_text='#c8c8c8'
+check_color='#3c76f2'
+fail_color='#f22424'
--- hyprland/hypr/scripts/hyprlock_time_anim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TIME=$(date +"%I:%M")
-SEC=$(date +"%S")
-AMPM=$(date +"%p")
-echo "$TIME<span size=\"102400\" foreground=\"#00ffff\">:$SEC</span><span size=\"61440\" foreground=\"#ffffff\"> $AMPM</span>"
